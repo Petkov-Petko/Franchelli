e19314a7b2113714fa9991639399ccdac9198a0a
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-22 21:40:51 +0300

    Refactor Footer component styles and add menu links

--- Franchelli/src/components/Footer/Footer.jsx
+++ Franchelli/src/components/Footer/Footer.jsx
@@ -6,25 +6,24 @@ const Footer = () => {
   return (
     <div className="footer">
       <img id="logoImgFooter" src={logo} alt="Logo"></img>
-      <div className="about">
-        <h3>About</h3>
-        <p>
-          Welcome to our website! We are dedicated to providing you with the
-          best experience possible. Browse through our wide selection of items
-          and explore all we have to offer.
-        </p>
-      </div>
-      <div className="Profile">
-        <h3>Profile</h3>
-        <p>Register</p>
-        <p>Login</p>
-        <p>Payment methods</p>
-      </div>
-      <div className="Contacts">
-        <h3>Contacts</h3>
-        <p>Address: 1234 Main Street</p>
-        <p>For orders:123-456-789</p>
-  
+      <div className="footer-menu">
+        <div className="about">
+          <h3>About</h3>
+          <p>
+            About
+          </p>
+        </div>
+        <div className="Profile">
+          <h3>Profile</h3>
+          <p>Register</p>
+          <p>Login</p>
+          <p>Payment methods</p>
+        </div>
+        <div className="Contacts">
+          <h3>Contacts</h3>
+          <p>Address: 1234 Main Street</p>
+          <p>For orders:123-456-789</p>
+        </div>
       </div>
       <div className="social">
         <div>
@@ -72,7 +71,9 @@ const Footer = () => {
           </a>
         </div>
       </div>
-      <div id="author"><p>© 2024 Restaurant FRANCHELLI. Website by Petko Petkov</p></div>
+      <div id="author">
+        <p>© 2024 Restaurant FRANCHELLI. Website by Petko Petkov</p>
+      </div>
     </div>
   );
 };
