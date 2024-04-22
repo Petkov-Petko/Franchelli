0e515a95f78b7c323bd3853f2bed38bd178279f7
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-22 19:37:17 +0300

    Add FoodView component and its styles

--- Franchelli/src/pages/Home/Home.jsx
+++ Franchelli/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header.jsx";
 import MenuList from "../../components/Menu/MenuList.jsx";
+import FoodView from "../../components/FoodView/FoodView.jsx";
 
 const Home = () => {
   const [category, setCategory] = useState("All");
@@ -10,6 +11,7 @@ const Home = () => {
     <div>
       <Header />
       <MenuList category={category} setCategory={setCategory} />
+      <FoodView category={category} />
     </div>
   );
 };
