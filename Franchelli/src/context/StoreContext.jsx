c57a2bc14bf72a7d8be6d4600d67d964ec9c2698
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-22 19:37:12 +0300

    Add StoreContext.jsx file for managing the store context and cart functionality

---
+++ Franchelli/src/context/StoreContext.jsx
@@ -0,0 +1,41 @@
+import { createContext, useEffect } from "react";
+import { food_list } from "../assets/assets";
+import { useState } from "react";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cardItems, setCardItems] = useState({});
+
+  const addToCart = (id) => {
+    if (!cardItems[id]) {
+      setCardItems((prev) => ({ ...prev, [id]: 1 }));
+    } else {
+      setCardItems((prev) => ({ ...prev, [id]: prev[id] + 1 }));
+    }
+  };
+
+  const removeFromCart = (id) => {
+    setCardItems((prev) => ({ ...prev, [id]: prev[id] - 1 }));
+  };
+
+  useEffect(() => {
+    console.log(cardItems);
+  }, [cardItems]);
+
+  const contextValue = {
+    food_list,
+    cardItems,
+    addToCart,
+    removeFromCart,
+    setCardItems,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
