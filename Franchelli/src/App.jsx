ae61c4f1f67f82555fc4840165cac74e845fe358
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-23 17:18:10 +0300

    Add LogIn component and update routing in App.jsx and main.jsx

--- Franchelli/src/App.jsx
+++ Franchelli/src/App.jsx
@@ -2,12 +2,19 @@ import { useState } from "react";
 import NavBar from "./components/navBar/navBar";
 import "./App.css";
 import Home from "./pages/Home/Home";
+import LogIn from "./components/LogIn/LogIn";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
+  const [isLogIn, setIsLogIn] = useState(false);
+
   return (
     <>
-      <NavBar />
-      <Home />
+      {isLogIn ? <LogIn setIsLogIn={setIsLogIn} /> : null}
+      <NavBar setIsLogIn={setIsLogIn} />
+      <Routes>
+        <Route path="/Franchelli/" element={<Home />} />
+      </Routes>
     </>
   );
 }
