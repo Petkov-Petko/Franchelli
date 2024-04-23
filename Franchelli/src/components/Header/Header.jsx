2f0a5c449e5a87713fcd22d8c55343a68f592e14
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-23 17:17:42 +0300

    Update Header component to include id attribute in div element

--- Franchelli/src/components/Header/Header.jsx
+++ Franchelli/src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
+    <div className="header" id="header">
       <div className="headerContent">
         <h1>Franchelli</h1>
         <h2>EXPERIENCE REAL ITALIAN CUISINE. </h2>
