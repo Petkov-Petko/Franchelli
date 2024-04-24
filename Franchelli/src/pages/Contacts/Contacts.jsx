54205cb186eef997cdb71e9f7085dee18e11cf14
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-24 16:33:26 +0300

    Add Contacts page and its CSS styles

---
+++ Franchelli/src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Contacts.css'
+
+const Contacts = () => {
+  return (
+    <div className='contacts'>
+        <div className='contactsDetails'>
+        <h2>Contacts</h2>
+            <h4>If you have any questions or just want to share your opinion, feel free to contact us at any time!</h4>
+            <h4>Opening hours of Fratelli restaurants:</h4>
+            <p>From 8 am to 23 p.m., every day</p>
+            <h4>Phones for reservations:</h4>
+            <p>Franchelli Varna /Evlogi Georgiev 47 Str./ -0123-421-421</p>
+            <h4>Emails to contact managers (for compliments, complaints or questions):</h4>
+            <p>manager@Franchelli.bg</p>
+        </div>
+    <div className='contactsFrom'>
+        <h3>Send us a message:</h3>
+        <form>
+            <input type='text' placeholder='Name'></input>
+            <input type='email' placeholder='Email'></input>
+            <textarea placeholder='Message'></textarea>
+            <button>Send</button>
+        </form>
+    </div>
+    </div>
+  )
+}
+
+export default Contacts
