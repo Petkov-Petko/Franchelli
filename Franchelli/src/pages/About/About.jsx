31aff7878b95b4d63fdc915fc276bd3b85c31771
Petko Petkov <petkozlatilov1234@gmail.com>
2024-04-24 16:02:07 +0300

    Refactor About component and its CSS styles

---
+++ Franchelli/src/pages/About/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="aboutPage">
+      <h1>About us</h1>
+      <p>
+        We are a family business that has been in the food industry for 30
+        years. We are proud to serve our customers with the best quality food
+        and service. Our goal is to provide the best experience for our
+        customers
+      </p>
+      <p>
+        We try to offer unique taste, recognizable atmosphere and pleasant
+        experiences to our customers. We do not compromise with the excellent
+        service, attention to detail and taste of the dishes!
+      </p>
+      <p>
+        In Fratelli restaurants you can have breakfast, lunch or dinner,
+        organize a company party, children's birthday, anniversary or a romantic
+        dinner. Make your reservation now.
+      </p>
+      <p>
+        Enjoy your favorite salads, pizza, specialties, pasta, seafood at home
+        or in the office - with a quick online order from our website.
+      </p>
+      <p id="training">
+        Please note that this website is created for training purposes only and the content displayed here is not intended for business or commercial use.
+      </p>
+    </div>
+  );
+};
+
+export default About;
